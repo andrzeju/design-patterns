cd3e99147dcbb97a5c699f25528f6c9f15d6a82f
andrzeju <andrzej.urban.pl@gmail.com>
2017-06-28 22:10:41 +0200

    factory pattern

---
+++ src/main/java/pl/urbanlab/patterns/creational/factory/Alcohol.java
@@ -0,0 +1,9 @@
+package pl.urbanlab.patterns.creational.factory;
+
+/**
+ * Created by Andrzej on 2017-06-28.
+ */
+public interface Alcohol {
+    String getName();
+    String getStrength();
+}
---
+++ src/main/java/pl/urbanlab/patterns/creational/factory/FactoryClient.java
@@ -0,0 +1,14 @@
+package pl.urbanlab.patterns.creational.factory;
+
+/**
+ * Created by Andrzej on 2017-06-28.
+ */
+public class FactoryClient {
+    public static void main(String[] args) {
+        Alcohol beer = AlcoholFactory.create("light");
+        System.out.println(beer.getName());
+
+        Alcohol vodka = AlcoholFactory.create("strong");
+        System.out.println(vodka.getName());
+    }
+}
